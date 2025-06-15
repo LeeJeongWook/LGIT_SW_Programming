6c2512acfac18818b9922ad1cb19f55822574e5f
JeongWook Lee <leejw0709@gmail.com>
2025-06-15 22:37:13 +0900

    Create 제품 진열.py

---
+++ 제품 진열.py
@@ -0,0 +1,36 @@
+import sys
+
+def Input_Data():
+	readl = sys.stdin.readline
+	N = int(readl())
+	ID = list(map(int, readl().split()))
+	return N, ID
+
+def CalLargestBlock(id_ignore):
+	max_block_size = 0
+	cur_size = 1
+	tmpID = [i for i in ID if i != id_ignore] #<<<
+	for i in range(1, len(tmpID)): #<<<
+		if tmpID[i] == tmpID[i - 1]: cur_size += 1 #<<<
+		else: cur_size = 1
+
+		if max_block_size < cur_size: max_block_size = cur_size
+	return max_block_size
+
+def Solve():
+	max_cnt = 0
+	sol = 10000000
+	for i in range(N):
+		ret = CalLargestBlock(ID[i])
+		if (max_cnt < ret) or ((max_cnt == ret) and (sol < ID[i])):
+			max_cnt = ret
+			sol = ID[i]
+	return sol
+
+# 입력 받는 부분
+N, ID = Input_Data()
+
+ans = Solve()
+
+# 출력하는 부분
+print(ans)
